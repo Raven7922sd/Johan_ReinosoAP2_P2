f1c695dd3b9527d2f2a470be918cf1425d377615
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-05 21:40:52 -0400

    appmodule

---
+++ app/src/main/java/edu/ucne/johan_reinoso_ap2_p2/local/module/AppModule.kt
@@ -0,0 +1,51 @@
+package edu.ucne.johan_reinoso_ap2_p2.local.module
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.johan_reinoso_ap2_p2.data.remoteApi.GastosApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://gestionhuacalesapi.azurewebsites.net/api/Gastos")
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideGastosApiService(retrofit: Retrofit): GastosApiService {
+        return retrofit.create(GastosApiService::class.java)
+    }
+}
